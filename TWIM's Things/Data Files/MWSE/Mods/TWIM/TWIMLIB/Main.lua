cc27649e0ff098f2974d4cbb4c7e8939a8e45591
TheWorldiMap <46360786+TheWorldiMap@users.noreply.github.com>
2025-02-08 17:34:01 +1000

    Update Main.lua

---
+++ TWIM's Things/Data Files/MWSE/Mods/TWIM/TWIMLIB/Main.lua
@@ -0,0 +1,18 @@
+------------------- stole this from seph till i translate the logger guide -----------------------
+local mod = "TWIMLIB"
+local version = "0.0.1"
+
+
+local function logMessage(message)
+	mwse.log("[" .. mod .. " " .. version .. "] " .. message)
+end
+----------------------------------------------------------------------------------------------------
+
+
+local function onInitiliasation(eventData)
+    event.register("load", onLoad)
+	event.register("loaded", onLoaded)
+    logMessage("Initliased")
+end
+
+event.register("initialized", onInitiliasation)
